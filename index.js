93eb013d4349a188427f2c523487f4d40818d746
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-03-03 12:41:34 -0800

    client set up

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import config from "./config.json" assert { type: "json" };
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
+client.login(config.token);
